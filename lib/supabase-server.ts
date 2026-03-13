144fc81957f3ab69f0c43a070c7728be9449fb09
xhris dior <diorrebero16@gmail.com>
2026-03-13 13:21:08 +0100

    Add type annotation for cookiesToSet parameter

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -9,7 +9,7 @@ export async function createServerSupabase() {
     {
       cookies: {
         getAll() { return cookieStore.getAll() },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: any[]) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
